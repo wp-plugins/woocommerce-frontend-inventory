5b203eed42a51de7961f5918524c62352ee79d8c
Rashef <Rashef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-29 10:44:06 +0000

    Tag 0.4
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-frontend-inventory/tags/0.4@733992 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ woocommerce-frontend-inventory.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Plugin Name: WooCommerce - Frontend Inventory
+Plugin URI: http://www.mirkogrewing.eu/woocommerce-frontend-inventory/
+Description: This plugin provides a template that can be applied to a page in order to show a full inventory of products in WooCommerce.
+Version: 0.4
+Author: Mirko Grewing
+Author URI: http://www.mirkogrewing.eu
+
+	Copyright: © 2013 Mirko Grewing (email : mirko@grewing.us)
+	License: GNU General Public License v3.0
+	License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+define('WOOFI_DIR', plugin_dir_path(__FILE__));
+define('WOOFI_URL', plugin_dir_url(__FILE__));
+
+function woofi_load(){
+		
+    if(is_admin()) //load admin files only in admin
+        require_once(WOOFI_DIR.'includes/admin.php');
+        
+    require_once(WOOFI_DIR.'includes/core.php');
+}
+woofi_load();
+
+?>
