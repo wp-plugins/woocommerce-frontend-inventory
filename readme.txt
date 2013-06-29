7e974f9dbe3ee236101dfc537308bb160d3a2eee
Rashef <Rashef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-29 10:00:25 +0000

    New settings area and admin.php specific file.
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-frontend-inventory/trunk@733971 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,24 +1,24 @@
 === WooCommerce Frontend Inventory ===
 Contributors: rashef
-Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=DYXGCQ7RUR6GA&lc=IT&item_name=Wordpress%20Plugins&item_number=woofi¤cy_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted
+Donate link: http://cl.ly/0m1h2L3u0K3Y
 Tags: WooCommerce, inventory, stock, variations
 Requires at least: 3.3
-Tested up to: 3.5.1
-Stable tag: 0.3
+Tested up to: 3.5.2
+Stable tag: 0.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-This plugin provides a shortcode that allows to print in a front-end page a full inventory of WooCommerce products, variants included.
+This plugin provides a shortcode that allows to print a full inventory of WooCommerce products, variants included, on a single frontend page.
 
 == Description ==
 
-Do you want to provide some users a **full inventory** of your WooCommerce installation without giving them access to the WooCommerce configuration area in the Dashboard?
+Do you want to provide some users a **full inventory** of your WooCommerce installation without giving them full-rights on your Wordpress?
 
-This plugin allows you to publish a full inventory on a frontend page with a simple shortcode. Not registered users will get an error message.
+This plugin allows you to publish a full inventory on a frontend page with a simple shortcode. Every registerd user (even subscribers) will be able to view it. Not registered users will get a customizable error message.
 
+**Please note** this is an early stage version of the plugin and it is very limited at the moment. It just prints the inventory in a page inside the template you are using (it might actually conflict with it for a couple of styles I hardcoded into the file).
 
-
-**Please note** this is an early stage version of the plugin and it won't offer any configurable setting or option. It just prints the inventory in a page inside the template you are using (it might actually conflict with it for a couple of styles I hardcoded into the file).
+Do you want to raise a bug, propose a change, request for support or simply congratulate me? The [Official Page][http://www.mirkogrewing.eu/woocommerce-frontend-inventory/ "Official Page of the Plugin"] is waiting for your voice!
 
 == Installation ==
 
@@ -30,26 +30,33 @@ This plugin allows you to publish a full inventory on a frontend page with a sim
 
 = Is there anything I can configure? =
 
-Not just yet.
+Just the error message. But hey... that's something! :P
 
-= It messes up with my template, what I can do? =
+= It messed up with my template, what I can do? =
 
-You are unlucky. The plugin uses a simple table and a couple of minimal styles. Unless you edit the styles directly in the plugin, you may want to wait for further releases: customization settings are on the roadmap...
+You are unlucky. The plugin uses a simple table and a couple of minimal styles. Unless you edit the styles directly in the plugin, you may want to wait for further releases: customizable styles are on the roadmap...
 
 = Hey, most of the visitors get an error message! =
 
-Only registered users can see that page. And yes, I will add an option for that eventually... :)
+Only registered users can see that page. You can modify the error message in the settings.
 
 == Changelog ==
 
+= v0.4 =
+* The plugin is now organized in a structured folder.
+* New settings area.
+* Customizable error message with HTML support.
+
+= v0.3 =
+* Minor fixes.
+
 = v0.2 =
-* Typos corrected
+* Typos corrected.
 
 = v0.1 =
-* First release
+* First release.
 
 == Upgrade notice ==
 
-= 0.2 =
-Just a few typos corrected.
+Say hi to the brand new Settings page! It is just a textarea to customize the error message for unauthorized fisitors, but here starts the serious stuff! :)
 
--- woocommerce-frontend-inventory.php
+++ woocommerce-frontend-inventory.php
@@ -2,131 +2,27 @@
 
 /*
 Plugin Name: WooCommerce - Frontend Inventory
-Plugin URI: 
+Plugin URI: http://www.mirkogrewing.eu/woocommerce-frontend-inventory/
 Description: This plugin provides a template that can be applied to a page in order to show a full inventory of products in WooCommerce.
-Version: 0.3
+Version: 0.4
 Author: Mirko Grewing
 Author URI: http://www.mirkogrewing.eu
+
+	Copyright: © 2013 Mirko Grewing (email : mirko@grewing.us)
+	License: GNU General Public License v3.0
+	License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
-function check_woo_notices() {
-	if (!is_plugin_active('woocommerce/woocommerce.php')) {
-        ob_start(); ?>
-	<div class="error">
-		<p><strong>WARNING</strong>: WooCommerce is not active and WooCommerce Front-End Inventory shortcode won't work!</p>
-	</div>
-	<?php
-	echo ob_get_clean();
-        }
+define('WOOFI_DIR', plugin_dir_path(__FILE__));
+define('WOOFI_URL', plugin_dir_url(__FILE__));
+
+function woofi_load(){
+		
+    if(is_admin()) //load admin files only in admin
+        require_once(WOOFI_DIR.'includes/admin.php');
+        
+    require_once(WOOFI_DIR.'includes/core.php');
 }
-
-add_action('admin_notices', 'check_woo_notices');
-
-function get_inventory() {
-	 $user = wp_get_current_user();
-	if ( empty( $user->ID ) ) {
-            echo 'Sorry you cannot access.';
-        }
-        else {
-            
-            global $woocommerce;
-    ?>
-        <style>
-            #reviews {display:none}
-        </style>
-        <table width="100%" style="border: 1px solid #000; width: 100%;" cellspacing="0" cellpadding="2">
-            <thead>
-                <tr>
-                    <th scope="col" style="text-align:left; border: 1px solid #000; padding: 6px;"><?php _e('Product', 'woothemes'); ?></th>
-                    <th scope="col" style="text-align:left; border: 1px solid #000; padding: 6px;"><?php _e('SKU', 'woothemes'); ?></th>
-                    <th scope="col" style="text-align:left; border: 1px solid #000; padding: 6px;"><?php _e('Stock', 'woothemes'); ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $args = array(
-                'post_type'         => 'product',
-                'post_status'       => 'publish',
-                'posts_per_page'    => -1,
-                'orderby'           => 'title',
-                'order'             => 'ASC',
-                'meta_query'        => array(
-                                            array(
-                                                'key'   => '_manage_stock',
-                                                'value' => 'yes'
-                                            )
-                                        ),
-                'tax_query'         => array(
-                                            array(
-                                                'taxonomy'  => 'product_type',
-                                                'field'     => 'slug',
-                                                'terms'     => array('simple'),
-                                                'operator'  => 'IN'
-                                            )
-                                        )
-                );
-                $loop = new WP_Query( $args );
-                while ( $loop->have_posts() ) : $loop->the_post();
-                global $product;
-                ?>
-                    <tr>
-                        <td style="text-align:left; border: 1px solid #000; padding: 6px;"><?php echo $product->get_title(); ?></td>
-                        <td style="text-align:left; border: 1px solid #000; padding: 6px;"><?php echo $product->sku; ?></td>
-                        <td style="text-align:left; border: 1px solid #000; padding: 6px;"><?php echo $product->stock; ?></td>
-                    </tr>
-                <?php
-                endwhile;
-                ?>
-            </tbody>
-        </table>
-        <h2>Variations</h2>
-        <table width="100%" style="border: 1px solid #000; width: 100%;" cellspacing="0" cellpadding="2">
-            <thead>
-                <tr>
-                    <th scope="col" style="text-align:left; border: 1px solid #000; padding: 6px;""><?php _e('Variation', 'woothemes'); ?></th>
-                    <th scope="col" style="text-align:left; border: 1px solid #000; padding: 6px;"><?php _e('Parent', 'woothemes'); ?></th>
-                    <th scope="col" style="text-align:left; border: 1px solid #000; padding: 6px;"><?php _e('SKU', 'woothemes'); ?></th>
-                    <th scope="col" style="text-align:left; border: 1px solid #000; padding: 6px;"><?php _e('Stock', 'woothemes'); ?></th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php
-            $args = array(
-                'post_type'         => 'product_variation',
-                'post_status'       => 'publish',
-                'posts_per_page'    => -1,
-                'orderby'           => 'title',
-                'order'             => 'ASC',
-                'meta_query'        => array(
-                                            array(
-                                                'key'   => '_stock',
-                                                'value' => array('', false, null),
-                                                'compare' => 'NOT IN'
-                                            )
-                                        )
-            );
-            $loop = new WP_Query( $args );
-            while ( $loop->have_posts() ) : $loop->the_post();
-                $product = new WC_Product_Variation( $loop->post->ID );
-            ?>
-                <tr>
-                    <td style="text-align:left; border: 1px solid #000; padding: 6px;"><?php echo $product->get_title(); ?></td>
-                    <td style="text-align:left; border: 1px solid #000; padding: 6px;"><?php echo get_the_title( $loop->post->post_parent ); ?></td>
-                    <td style="text-align:left; border: 1px solid #000; padding: 6px;"><?php echo $product->sku; ?></td>
-                    <td style="text-align:left; border: 1px solid #000; padding: 6px;"><?php echo $product->stock; ?></td>
-                </tr>
-            <?php
-            endwhile;
-            ?>
-            </tbody>
-        </table>
-<?php
-            
-            
-            
-       }
-     }
-
-add_shortcode( 'woofi', 'get_inventory' );
+woofi_load();
 
 ?>
